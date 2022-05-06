1144c0d08b4cf971c07aaf6e8279bd25c482b5a7
3KU_Delta <48127424+3KUdelta@users.noreply.github.com>
2022-05-06 12:39:41 +0200

    Update settings.h

--- settings.h
+++ settings.h
@@ -1,7 +1,26 @@
-String ver = "Version 1.0";
+/**************************************************************************
+  Settings for VFD Futuba - Type No. 8-MD-06INKM
+  SPI connection on MOSI, CLK and CS
+  SPI according to datasheet: 0.5 MhZ, clock high idle, data on rising, LBSfirst
 
-uint8_t din   = 23; // DA (MOSI)
-uint8_t clk   = 18; // CK (CLK)
-uint8_t cs    = 5;  // CS (SS)
+  8 or 16 bits (digits) display
+  Brigthness 0 - 255
 
+  MIT License
+
+  Copyright (c) 2022 Marc Stähli
+
+  https://github.com/3KUdelta/Futaba-VFD-16bit_ESP32
+
+ ***************************************************************************/
+String ver = "Version 1.2";
+unsigned int digits = 16;      // either 8 or 16 digits for the Futaba VFD
 unsigned int brightness = 50;  // initial brightness settings
+
+//****Do not change below this line *****************************************
+static const int spiClk = 100000;  // 0.1 MHz
+#define VSPI_MISO      MISO        // not relevant can be left unplugged
+#define VSPI_MOSI      MOSI        // ESP32 Pin 23
+#define VSPI_SCLK      SCK         // ESP32 Pin 18
+#define VSPI_SS        SS          // EsP32 Pin 5
+#define VSPI_SETTINGS  SPISettings(spiClk, LSBFIRST, SPI_MODE3)
