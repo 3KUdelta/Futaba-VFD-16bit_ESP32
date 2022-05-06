022a36b569be1050b30309694340939124493467
3KU_Delta <48127424+3KUdelta@users.noreply.github.com>
2022-05-06 12:39:08 +0200

    using SPI.h for more stability

--- vfd_controls.h
+++ vfd_controls.h
@@ -1,76 +1,57 @@
-void write_byte(byte w_data) {
-  for (int i = 0; i < 8; i++)
-  {
-    digitalWrite(clk, LOW);
-    if ((w_data & 0x01) == 0x01)
-    {
-      digitalWrite(din, HIGH);
-    }
-    else
-    {
-      digitalWrite(din, LOW);
-    }
-    w_data >>= 1;
-    digitalWrite(clk, HIGH);
-  }
-  delay(1);
-}
+SPIClass * vspi = NULL;           //uninitalised pointers to SPI objects
+
 void VFD_brightness(unsigned int level) {
-  digitalWrite(cs, LOW);
-  write_byte(0xe4);          //byte address for setting brightness
-  delayMicroseconds(5);
-  write_byte(level);         //level 255 max
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  vspi->transfer(0xe4);            //first byte address for setting digits
+  vspi->transfer(level);            //8 digits equals value 7 (hex 0x07)
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 void VFD_clearScreen(void) {
-  digitalWrite(cs, LOW);
-  write_byte(0x20);
-  for (int i = 0; i < 16; i++) {
-    write_byte(0x20);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  vspi->transfer(0x20);
+  for (int i = 0; i < digits; i++) {
+    vspi->transfer(0x20);
   }
-  write_byte(0xe8);  // refresh
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
+  vspi->transfer(0xe8);  // refresh
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 void VFD_show(void) {
-  delay(10);
-  digitalWrite(cs, LOW);
-  delayMicroseconds(5);
-  write_byte(0xe8);
-  digitalWrite(cs, HIGH);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  vspi->transfer(0xe8);
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 void VFD_standby(bool on) {
-  digitalWrite(cs, LOW);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
   if (on) {
-    write_byte(0xed);     // standby mode
+    vspi->transfer(0xed);     // standby mode
   }
   else {
-    write_byte(0xec);     // running mode
+    vspi->transfer(0xec);     // running mode
   }
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
-}
-void VFD_cmd(byte command) {
-  digitalWrite(cs, LOW);
-  write_byte(command);
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 void VFD_WriteASCII(int x, unsigned char chr) {
-  digitalWrite(cs, LOW);
-  write_byte(0x20 + x);
-  delayMicroseconds(5);
-  write_byte(chr);
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  vspi->transfer(0x20 + x);
+  vspi->transfer(chr);
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 String translateSpecialChars(String str) {
   unsigned int l = str.length();   // how many chars were sent?
   char bla[l];                     // define working array of chars
   str.toCharArray(bla, l + 1);     // move string into array of chars
   for (int i = 0; i < l; i++) {
-    if (int(bla[i]) == 195) {      // special char is coming (first is 195)
+    if (int(bla[i]) == 195) {      // special char is being announced (first is 195)
       bla[i] = 255;                // filling with marker (255 not used)
       switch (int(bla[i + 1])) {
         case 132: bla[i + 1] = 196; break; //Ä
@@ -81,7 +62,7 @@ String translateSpecialChars(String str) {
         case 188: bla[i + 1] = 252; break; //ü
       }
     }
-    if (int(bla[i]) == 194) {      // special char is coming (first is 194)
+    if (int(bla[i]) == 194) {      // special char is announced (first is 194)
       bla[i] = 255;                // filling with marker (255 not used)
       bla[i + 1] = 176;            // Degree sign (°)
     }
@@ -106,62 +87,69 @@ void VFD_WriteStr(int x, String str) {
   unsigned int l = str.length();   // how many chars were sent?
   char bla[l];                     // define working array of chars
   str.toCharArray(bla, l + 1);     // move string into array of chars
-  //Serial.print("Sending to display: ");
-  //Serial.println(bla);
+  Serial.print("Sending to display: ");
+  Serial.println(bla);
 
-  digitalWrite(cs, LOW);
-  delayMicroseconds(10);
-  write_byte(0x20);                // base register DCRAM 0H
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  vspi->transfer(0x20);                // base register DCRAM 0H
 
-  if (l > 16) {
-    for (int i = 0; i < 16 - x; i++) {
-      write_byte(bla[i]);         //  fill first batch to end
+  if (l > digits) {
+    for (int i = 0; i < digits - x; i++) {
+      vspi->transfer(bla[i]);         // fill first batch to end of visible chars
     }
-    digitalWrite(cs, HIGH);
-    delay(1000);
-    for (int i = 0; i < l - 16; i++) {
-      digitalWrite(cs, LOW);
-      delayMicroseconds(10);
-      write_byte(0x20);                // base register DCRAM 0H
-      for (int ii = 0; ii < 16; ii++) {
-        write_byte(bla[ii + 1 + i]);   // shift (scroll to left)
+    digitalWrite(VSPI_SS, HIGH);
+    delay(1000);                    // pause for reading
+    for (int i = 0; i < l - digits; i++) {
+      digitalWrite(VSPI_SS, LOW);
+      vspi->transfer(0x20);                // base register DCRAM 0H
+      for (int ii = 0; ii < digits; ii++) {
+        vspi->transfer(bla[ii + 1 + i]);   // shift (scroll to left)
       }
-      digitalWrite(cs, HIGH);
-      delay(100);
+      digitalWrite(VSPI_SS, HIGH);
+      delay(100);                 // delay time for scrolling
     }
   }
   else {
     for (int i = 0; i < l; i++) {
-      write_byte(bla[i]);
+      vspi->transfer(bla[i]);
     }
-    for (int i = 0; i < 16 - x - l; i++) {
-      write_byte(0x20);            // fill with blanks
+    for (int i = 0; i < digits - x - l; i++) {
+      vspi->transfer(0x20);            // fill ends with blanks
     }
-    digitalWrite(cs, HIGH);
   }
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
 }
 void VFD_init() {
   /* Setting Digits ****************************************************/
-  digitalWrite(cs, LOW);
-  write_byte(0xe0);            //first byte address for setting digits
-  delayMicroseconds(5);
-  write_byte(0x0f);            //16 digits equals value 15 (hex 0x0f)
-  digitalWrite(cs, HIGH);
-  delayMicroseconds(5);
+  vspi->beginTransaction(VSPI_SETTINGS);
+  digitalWrite(VSPI_SS, LOW);
+  if (digits == 8) {
+    vspi->transfer(0xe0);            //first byte address for setting digits
+    vspi->transfer(0x07);            //8 digits equals value 7 (hex 0x07)
+  }
+  if (digits == 16) {
+    vspi->transfer(0xe0);            //first byte address for setting digits
+    vspi->transfer(0x0f);            //8 digits equals value 7 (hex 0x07)
+  }
+  digitalWrite(VSPI_SS, HIGH);
+  vspi->endTransaction();
   /* *******************************************************************/
-  VFD_brightness(brightness);  //setting brightness to initial values
-  VFD_standby(false);          //switch standby off
-  VFD_clearScreen();           //clear the screen
-  VFD_show();                  //activate the screen
+  VFD_brightness(brightness);      //setting brightness to initial values
+  VFD_standby(false);              //switch standby off
+  VFD_clearScreen();               //clear the screen
+  VFD_show();                      //activate the screen
   /* Quick test of all digits ******************************************/
   for (int i = 10; i-- > 0;) {
-    for (int ii = 0; ii < 16; ii++) {
+    for (int ii = 0; ii < digits; ii++) {
       VFD_WriteASCII(ii, i + 0x30);
-      delay(10);
+      delay(8);
     }
   }
   /* *******************************************************************/
-  VFD_clearScreen();           //clear the screen
-  VFD_WriteStr(2, ver);        //write version number to screen
+  VFD_clearScreen();               //clear the screen
+  VFD_WriteStr(2, ver);            //write version number to screen
   delay(1000);
+  VFD_clearScreen();               //clear the screen
 }
